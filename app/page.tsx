de4ea03b2068d8bad8f65be11e649ff37f4278f1
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-10-27 13:52:05 +0530

    added some css in btn and changed bg color

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
-      <div className="w-full max-w-[600px] mx-auto">
-        <h1 className=" text-6xl mb-4">The best journal app, period.</h1>
+    <div className="w-screen h-screen bg-[#121212] flex justify-center items-center text-white">
+      <div className="w-full max-w-[650px] mx-auto">
+        <h1 className=" text-6xl mb-4">The Best Journal App, period.</h1>
         <p className=" text-2xl text-white/60 mb-4">
           This is the best app for tracking how you feel throughout your life.
           All you have to do is be honest.
         </p>
         <div>
-          <button className="bgblue600">get started</button>
+          <Link href="/">
+            <button className=" bg-blue-600 p-4 rounded-lg text-xl hover:bg-blue-600/30">
+              Get started
+            </button>
+          </Link>
         </div>
       </div>
     </div>
