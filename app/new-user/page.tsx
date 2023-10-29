e11c7b49bb033cbf37a7a61bb229768b913259cb
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-10-29 12:47:14 +0530

    completed auth

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-const NewUser = () => {
-  return <div>hi</div>
+const createNewUser = async () => {
+  const user = await currentUser()
+  console.log(user)
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    })
+  }
+
+  redirect('/journal')
+}
+
+const NewUser = async () => {
+  await createNewUser()
+  return <div>...loading</div>
 }
 
 export default NewUser
