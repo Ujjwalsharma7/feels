a7a6e69c2875ee984c99543992ab330bc0a0160a
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-10-29 13:08:10 +0530

    fixed a bug

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import { auth } from '@clerk/nextjs'
 export default async function Home() {
   const { userId } = await auth()
 
-  let href = (await auth()).userId ? '/journal' : '/new-user'
+  let href = userId ? '/journal' : '/new-user'
   return (
-    <div className="w-screen h-screen bg-[#121212] flex justify-center items-center text-white">
+    <div className="w-screen h-screen bg-[#121212] flex justify-center items-center text-white p-6">
       <div className="w-full max-w-[650px] mx-auto">
         <h1 className=" text-6xl mb-4">The Best Journal App, period.</h1>
         <p className=" text-2xl text-white/60 mb-4">
